c677385da82e64c0dc3261dff96385926e93b1e6
palo Solidale <palosolidaleeverde@gmail.com>
2020-09-05 01:11:21 +0200

    Fix branches

---
+++ src/app/candidates/candidates.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidates',
+  templateUrl: './candidates.component.html',
+  styleUrls: ['./candidates.component.scss']
+})
+export class CandidatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  prova(): void {
+    document.getElementById('carousl').scrollIntoView();
+  }
+}
